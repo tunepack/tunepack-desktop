27bf5f09ae2885a7c450789c10339d13f6be1722
Team Tunepack <team.tunepack@gmail.com>
2019-05-24 22:02:45 +0200

    Nav position can now be configured, and moved it to the bottom

--- src/main/config.js
+++ src/main/config.js
@@ -4,7 +4,7 @@ const stylingVariables = {
   WINDOW_WIDTH: '540px',
   WINDOW_HEIGHT: '720px',
   HEADER_HEIGHT: '40px',
-  NAV_HEIGHT: '50px',
+  NAV_HEIGHT: '60px',
   SIDE_PADDING: '12px',
   LIST_ITEM_HEIGHT: '100px'
 }
